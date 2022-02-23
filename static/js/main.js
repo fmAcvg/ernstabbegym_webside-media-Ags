d7cc6fe76a8c6d43e9a916f240d86afe7fa805b4
lars <fly21@gmx.de>
2022-02-23 12:20:43 -0600

    updated

---
+++ static/js/main.js
@@ -0,0 +1,24 @@
+function reveal() {
+    const reveals = document.querySelectorAll(".reveal");
+
+    for (let i = 0; i < reveals.length; i++) {
+        const windowHeight = window.innerHeight;
+        const elementTop = reveals[i].getBoundingClientRect().top;
+        const elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+function opennav() {
+
+    document.getElementsByClassName("navbar")[0].style.top = "34px";
+    document.getElementsByClassName("navbar")[0].style.height = "50px";
+}
+
